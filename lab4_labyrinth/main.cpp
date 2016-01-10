cd33c8abbc3a7f8b9da43f91be720e3ad6615684
BeeHappy <pianonouta@gmail.com>
2016-01-10 23:32:22 +0200

    Update main.cpp

--- lab4_labyrinth/main.cpp
+++ lab4_labyrinth/main.cpp
@@ -1,148 +1,4 @@
-/*#include <SDL/SDL.h>
-#include <SDL2/SDL.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
 
-void drawCube(float size)
-{
-        float difamb[] = {1.0,0.5,0.3,1.0};
-        glBegin(GL_QUADS);
-                glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, difamb);
-                // front face
-                glNormal3f(0.0,0.0,1.0);
-//              glColor3f(1.0,0.0,0.0);
-                glVertex3f(size/2,size/2,size/2);
-                glVertex3f(-size/2,size/2,size/2);
-                glVertex3f(-size/2,-size/2,size/2);
-                glVertex3f(size/2,-size/2,size/2);
-                // left face
-                glNormal3f(-1.0,0.0,0.0);
-//              glColor3f(0.0,1.0,0.0);
-                glVertex3f(-size/2,size/2,size/2);
-                glVertex3f(-size/2,-size/2,size/2);
-                glVertex3f(-size/2,-size/2,-size/2);
-                glVertex3f(-size/2,size/2,-size/2);
-                // back face
-                glNormal3f(0.0,0.0,-1.0);
-//              glColor3f(0.0,0.0,1.0);
-                glVertex3f(size/2,size/2,-size/2);
-                glVertex3f(-size/2,size/2,-size/2);
-                glVertex3f(-size/2,-size/2,-size/2);
-                glVertex3f(size/2,-size/2,-size/2);
-                // right face
-                glNormal3f(1.0,0.0,0.0);
-//              glColor3f(1.0,1.0,0.0);
-                glVertex3f(size/2,size/2,size/2);
-                glVertex3f(size/2,-size/2,size/2);
-                glVertex3f(size/2,-size/2,-size/2);
-                glVertex3f(size/2,size/2,-size/2);
-                // top face
-                glNormal3f(0.0,1.0,0.0);
-//              glColor3f(1.0,0.0,1.0);
-                glVertex3f(size/2,size/2,size/2);
-                glVertex3f(-size/2,size/2,size/2);
-                glVertex3f(-size/2,size/2,-size/2);
-                glVertex3f(size/2,size/2,-size/2);
-                // bottom face
-                glNormal3f(0.0,-1.0,0.0);
-//              glColor3f(0.0,1.0,1.0);
-                glVertex3f(size/2,-size/2,size/2);
-                glVertex3f(-size/2,-size/2,size/2);
-                glVertex3f(-size/2,-size/2,-size/2);
-                glVertex3f(size/2,-size/2,-size/2);
-        glEnd();
-}
-
-float angle = 0.0;
-const int triangle = 1;
-
-unsigned int loadTexture(const char* filename)
-{
-        SDL_Surface* img = SDL_LoadBMP(filename);
-        unsigned int id;
-        glGenTextures(1, &id);
-        glBindTexture(GL_TEXTURE_2D, id);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->w, img->h, 0, GL_RGB, GL_UNSIGNED_SHORT_5_6_5, img->pixels);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        SDL_FreeSurface(img);
-        return id;
-}
-
-unsigned int tex;
-
-void init()
-{
-        glClearColor(0.0,0.0,0.0,1.0);  //background color and alpha
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        gluPerspective(45,640.0/480.0,1.0,500.0);
-        glMatrixMode(GL_MODELVIEW);
-        glEnable(GL_DEPTH_TEST);
-        glEnable(GL_TEXTURE_2D);
-        tex = loadTexture("brick.bmp");
-}
-
-void display()
-{
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-        glLoadIdentity();
-        glTranslatef(0.0,0.0,-5.0);
-        glRotatef(angle,1.0,1.0,1.0);   // angle, x-axis, y-axis, z-axis
-        glBindTexture(GL_TEXTURE_2D, tex);
-        glBegin(GL_QUADS);
-                glTexCoord2f(0.0,2.0);
-                glVertex3f(-2.0,2.0,0.0);
-                glTexCoord2f(0.0,0.0);
-                glVertex3f(-2.0,-2.0,0.0);
-                glTexCoord2f(2.0,0.0);
-                glVertex3f(2.0,-2.0,0.0);
-                glTexCoord2f(2.0,2.0);
-                glVertex3f(2.0,2.0,0.0);
-        glEnd();
-}
-
-int main(int argc, char** argv)
-{
-        SDL_Init(SDL_INIT_EVERYTHING);
-        SDL_Surface *screen;
-        screen = SDL_SetVideoMode(1024, 768, 32, SDL_SWSURFACE|SDL_OPENGL);
-        bool running = true;
-        const int FPS = 30;
-        Uint32 start;
-        SDL_Event event;
-        init();
-        while(running) {
-                start = SDL_GetTicks();
-                while(SDL_PollEvent(&event)) {
-                        switch(event.type) {
-                                case SDL_QUIT:
-                                        running = false;
-                                        break;
-                                case SDL_KEYDOWN:
-                                        switch(event.key.keysym.sym)
-                                        {
-                                                case SDLK_ESCAPE:
-                                                        running = false;
-                                                        break;
-                                        }
-                                        break;
-                        }
-                }
-
-                display();
-                SDL_GL_SwapBuffers();
-                angle += 0.5;
-                if(angle > 360)
-                        angle -= 360;
-                if(1000/FPS > SDL_GetTicks()-start)
-                        SDL_Delay(1000/FPS-(SDL_GetTicks()-start));
-        }
-        SDL_Quit();
-        return 0;
-}
-
-*/
 #include "input.h"
 #include <QCoreApplication>
 #include "labyrinth.h"
@@ -156,10 +12,6 @@ int main(int argc, char** argv)
 //SOIL
 #include <SOIL/SOIL.h>
 
-// GLEW
-#define GLEW_STATIC
-#define GL_CLAMP_TO_EDGE 0x812F
-#include <GL/glew.h>
 #include <GL/glut.h>
 
 #include <GL/gl.h>
@@ -181,15 +33,8 @@ input in;
 void redrawTimer(int);
 void move();
 string solve();
-//void key_callback(GLFWwindow *window, int key, int, int action, int);
-//void mouse_callback(GLFWwindow *, double xpos, double ypos);
-//void scrool_callback(GLFWwindow *, double, double yoffset);
 
 void initialize();
-
-//#include "camera.h"
-//static Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-
 
 typedef struct _AUX_RGBImageRec {
    GLint sizeX, sizeY;
@@ -455,18 +300,9 @@ void initialize() {
     float pos[] = {200, 200, 200, 1.0};
     glLightfv(GL_LIGHT0, GL_POSITION, pos);
 
-
-//    glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//    gluPerspective(160, 1, 50, 0);
-
-
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(500, 400, 500, 300, 0, 300, -50, 0, -50);
-
-
-//gluLookAt(eyex, eyey, eyez, centerx, centery, centerz, norx, nory, norz);
 
     glMatrixMode(GL_MODELVIEW);
     glEnable(GL_DEPTH_TEST);
